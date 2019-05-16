844e6883106ff9c05cab7b1a5ab052b9df596648
Donald Shen <donaldmyshen@outlook.com>
2019-05-16 15:32:49 -0500

    original

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const PROFILE_PIC_URL_PREFIX =
+    'https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190';
+export const TEAM_PIC_URL_PREFIX =
+    'http://stats.nba.com/media/img/teams/logos';
+
+export const DEFAULT_PLAYER_INFO = {
+    // playe ID?
+    playerId: 201939,
+    fullName: 'Lebron James',
+    teamAbbreviation: 'LAL',
+};
